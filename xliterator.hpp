d06c5fab9cbed7502b06672bf5d4e83d4f133994
Janez Žemva <janezz55@gmail.com>
2021-10-25 10:17:30 +0200

    some fixes

--- xliterator.hpp
+++ xliterator.hpp
@@ -56,19 +56,12 @@ public:
   xliterator& operator=(xliterator const&) = default;
   xliterator& operator=(xliterator&&) = default;
 
-  bool operator==(auto const& o) const noexcept
-    requires(
-      std::is_same_v<std::remove_cvref_t<decltype(o)>, xliterator> ||
-      std::is_same_v<std::remove_cvref_t<decltype(o)>, inverse_const_t>
-    )
+  bool operator==(xliterator const& o) const noexcept
   {
     return n_ == o.n_;
   }
 
-  bool operator!=(auto&& o) const noexcept
-  {
-    return !(*this == std::forward<decltype(o)>(o));
-  }
+  bool operator!=(xliterator const& o) const noexcept = default;
 
   // increment, decrement
   auto& operator++() noexcept
