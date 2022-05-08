2addad276e27a9112e2b47ac8f2048465c9f0adc
Janez Žemva <janezz55@gmail.com>
2022-05-08 06:52:41 +0200

    some fixes

--- list.hpp
+++ list.hpp
@@ -28,10 +28,10 @@ public:
   using reference = value_type&;
   using const_reference = value_type const&;
 
-  using const_iterator = listiterator<node const>;
-  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
   using iterator = listiterator<node>;
   using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_iterator = listiterator<node const>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 private:
   struct node
