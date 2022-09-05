79cafe9d18cadc0258695b1b6ba5ec9e359c52ec
Janez Žemva <janezz55@gmail.com>
2022-09-05 22:15:29 +0200

    some fixes

--- list.hpp
+++ list.hpp
@@ -309,7 +309,7 @@ public:
   //
   iterator emplace(const_iterator const i, auto&& ...a)
     noexcept(noexcept(new node(std::forward<decltype(a)>(a)...)))
-    requires(std::is_constructible_v<value_type, decltype(a)...>)
+    requires(std::is_constructible_v<value_type, decltype(a)&&...>)
   {
     auto const n(i.n());
     auto const p(n ? i.p() : last_);
@@ -341,7 +341,7 @@ public:
 
   iterator emplace_back(auto&& ...a)
     noexcept(noexcept(new node(std::forward<decltype(a)>(a)...)))
-    requires(std::is_constructible_v<value_type, decltype(a)...>)
+    requires(std::is_constructible_v<value_type, decltype(a)&&...>)
   {
     auto const l(last_);
 
@@ -363,7 +363,7 @@ public:
 
   iterator emplace_front(auto&& ...a)
     noexcept(noexcept(new node(std::forward<decltype(a)>(a)...)))
-    requires(std::is_constructible_v<value_type, decltype(a)...>)
+    requires(std::is_constructible_v<value_type, decltype(a)&&...>)
   {
     auto const f(first_);
 
