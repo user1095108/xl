5e6113424fc5733757a08f97db00ff9e458c072e
user1095108 <janezz55@gmail.com>
2023-10-23 13:53:11 +0200

    some fixes

--- listiterator.hpp
+++ listiterator.hpp
@@ -12,10 +12,10 @@ template <typename T>
 class listiterator
 {
   using inverse_const_t = std::conditional_t<
-    std::is_const_v<T>,
-    listiterator<std::remove_const_t<T>>,
-    listiterator<T const>
-  >;
+      std::is_const_v<T>,
+      listiterator<std::remove_const_t<T>>,
+      listiterator<T const>
+    >;
 
   friend inverse_const_t;
 
@@ -26,10 +26,10 @@ public:
   using iterator_category = std::bidirectional_iterator_tag;
   using difference_type = std::intmax_t;
   using value_type = std::conditional_t<
-    std::is_const_v<T>,
-    typename T::value_type const,
-    typename T::value_type
-  >;
+      std::is_const_v<T>,
+      typename T::value_type const,
+      typename T::value_type
+    >;
 
   using pointer = value_type*;
   using reference = value_type&;
