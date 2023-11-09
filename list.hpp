b3c9a25712bd8d75640278848809da45c43a3de6
user1095108 <janezz55@gmail.com>
2023-11-09 17:28:00 +0100

    some fixes

--- list.hpp
+++ list.hpp
@@ -108,7 +108,7 @@ public:
 
   list(list&& o) noexcept:
     f_(o.f_), l_(o.l_)
-  { // we are empty, so no need for clear()
+  { // we are empty, so no need to clear()
     o.f_ = o.l_ = {};
   }
 
