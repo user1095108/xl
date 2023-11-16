64d404d7f0c9bcffeafe75f0feb58bb27e337347
user1095108 <janezz55@gmail.com>
2023-11-16 17:49:23 +0100

    some fixes

--- misctests.cpp
+++ misctests.cpp
@@ -5,14 +5,20 @@ void print(int id, const xl::list<int>& container)
 {
   std::cout << id << ". ";
   for (const int x : container)
-      std::cout << x << ' ';
+    std::cout << x << ' ';
   std::cout << '\n';
 }
- 
+
+void print_container(const xl::list<int>& c) 
+{
+  for (int i : c)
+    std::cout << i << ' ';
+  std::cout << '\n';
+}
+
 int main ()
 {
-  {
-  // https://en.cppreference.com/w/cpp/container/list/insert
+  { // https://en.cppreference.com/w/cpp/container/list/insert
   xl::list<int> c1(3, 100);
   print(1, c1);
 
@@ -36,6 +42,32 @@ int main ()
 
   c1.insert(c1.end(), {601, 602, 603});
   print(6, c1);
+  }
+
+  { // https://en.cppreference.com/w/cpp/container/list/erase
+  xl::list<int> c{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  print_container(c);
+
+  c.erase(c.begin());
+  print_container(c);
+
+  auto range_begin = c.begin();
+  auto range_end = c.begin();
+  std::advance(range_begin, 2);
+  std::advance(range_end, 5);
+
+  c.erase(range_begin, range_end);
+  print_container(c);
+
+  // Erase all even numbers
+  for (auto it = c.begin(); it != c.end();)
+  {
+    if (*it % 2 == 0)
+      it = c.erase(it);
+    else
+      ++it;
+  }
+  print_container(c);
   }
 
   return 0;
