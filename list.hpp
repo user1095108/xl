cd592cb35aecbe928d0cfe353b8ffe5a56d868a3
user1095108 <janezz55@gmail.com>
2023-11-16 19:04:55 +0100

    some fixes

--- list.hpp
+++ list.hpp
@@ -471,7 +471,6 @@ public:
 
   auto insert(multi_t, const_iterator const i, value_type a)
     noexcept(noexcept(insert<0>(multi_t{}, i, std::move(a))))
-    requires(requires{insert<0>(multi_t{}, i, std::move(a));})
   {
     return insert<0>(multi_t{}, i, std::move(a));
   }
