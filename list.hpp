8ec48a8d4204eebbe3bca9d35825f889d552e063
user1095108 <janezz55@gmail.com>
2023-11-21 19:57:13 +0100

    some fixes

--- list.hpp
+++ list.hpp
@@ -76,7 +76,8 @@ private:
 
     static auto next(auto n, size_type i) noexcept
     {
-      for (decltype(n) p{}; i--;) assign(n, p)(n->link(p), n); return n;
+      for (decltype(n) p{}; i--; assign(n, p)(n->link(p), n));
+      return n;
     }
 
     //
