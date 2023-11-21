58397ee9cbc040e1f1106894432c46b6e3b24583
user1095108 <janezz55@gmail.com>
2023-11-21 05:20:30 +0100

    some fixes

--- misctests.cpp
+++ misctests.cpp
@@ -70,6 +70,27 @@ int main ()
   print_container(c);
   }
 
+  { // https://en.cppreference.com/w/cpp/container/list
+  // Create a list containing integers
+  xl::list<int> l = {7, 5, 16, 8};
+
+  // Add an integer to the front of the list
+  l.push_front(25);
+  // Add an integer to the back of the list
+  l.push_back(13);
+
+  // Insert an integer before 16 by searching
+  auto it = std::find(l.begin(), l.end(), 16);
+  if (it != l.end())
+      l.insert(it, 42);
+
+  // Print out the list
+  std::cout << "l = { ";
+  for (int n : l)
+      std::cout << n << ", ";
+  std::cout << "};\n";
+  }
+
   return 0;
 }
 
