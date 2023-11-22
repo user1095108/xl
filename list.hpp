01003cb9eae6c05c579acfd7e0d168b6a7552701
user1095108 <janezz55@gmail.com>
2023-11-22 18:38:18 +0100

    some fixes

--- list.hpp
+++ list.hpp
@@ -692,21 +692,21 @@ public:
     node::assign(f_, l_)(b.n_, e.p_);
   }
 
-  void splice(const_iterator i, auto&& o, const_iterator b,
+  void splice(const_iterator const i, auto&& o, const_iterator const b,
     const_iterator const e) noexcept
     requires(std::same_as<list, std::remove_reference_t<decltype(o)>>)
   {
     if (b != e)
-    {
+    { // relink b, e, i
       i.p_ ? i.p_->l_ ^= node::conv(i.n_, b.n_) : bool(f_ = b.n_);
       i.n_ ? i.n_->l_ ^= node::conv(i.p_, e.p_) : bool(l_ = e.p_);
 
       //
+      b.p_ ? b.p_->l_ ^= node::conv(b.n_, e.n_) : bool(o.f_ = e.n_);
       b.n_->l_ ^= node::conv(i.p_, b.p_);
-      e.p_->l_ ^= node::conv(e.n_, i.n_);
 
       //
-      b.p_ ? b.p_->l_ ^= node::conv(b.n_, e.n_) : bool(o.f_ = e.n_);
+      e.p_->l_ ^= node::conv(e.n_, i.n_);
       e.n_ ? e.n_->l_ ^= node::conv(b.p_, e.p_) : bool(o.l_ = b.p_);
     }
   }
