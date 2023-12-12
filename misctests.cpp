f5afaf3c3804be6f8d9218d29f08097f791e65a3
user1095108 <janezz55@gmail.com>
2023-12-12 16:40:02 +0100

    some fixes

--- misctests.cpp
+++ misctests.cpp
@@ -116,6 +116,19 @@ int main ()
   std::cout << "list2:" << list2 << '\n';
   }
 
+  { // https://en.cppreference.com/w/cpp/container/list/merge
+  xl::list<int> list1 = {5, 9, 1, 3, 3};
+  xl::list<int> list2 = {8, 7, 2, 3, 4, 4};
+
+  list1.sort();
+  list2.sort();
+  std::cout << "list1: " << list1 << '\n';
+  std::cout << "list2: " << list2 << '\n';
+
+  list1.merge(list2);
+  std::cout << "merged:" << list1 << '\n';
+  }
+
   return 0;
 }
 
