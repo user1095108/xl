81fe7d695dfc8665995d4f597121dee67966004d
user1095108 <janezz55@gmail.com>
2023-12-13 18:05:55 +0100

    some fixes

--- misctests.cpp
+++ misctests.cpp
@@ -152,5 +152,24 @@ int main ()
   std::cout << "Erased even numbers: " << erased << '\n';
   }
 
+  { // https://en.cppreference.com/w/cpp/container/list/unique
+  xl::list<int> c{1, 2, 2, 3, 3, 2, 1, 1, 2};
+  std::cout << "Before unique(): " << c << std::endl;
+  const auto count1 = c.unique();
+  std::cout << "After unique():  " << c << "\n"
+            << count1 << " elements were removed\n";
+
+  c = {1, 2, 12, 23, 3, 2, 51, 1, 2, 2};
+  std::cout << "\nBefore unique(pred): " << c << std::endl;
+
+  const auto count2 = c.unique([mod = 10](int x, int y)
+  {
+      return (x % mod) == (y % mod);
+  });
+
+  std::cout << "After unique(pred):  " << c << "\n"
+            << count2 << " elements were removed\n";
+  }
+
   return 0;
 }
