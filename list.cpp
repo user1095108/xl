76c6afcae063cd82516820fe0a05b74cee3dacc5
user1095108 <janezz55@gmail.com>
2023-12-17 12:02:50 +0100

    some fixes

--- list.cpp
+++ list.cpp
@@ -11,7 +11,7 @@ int main()
   l.push_back(0ull, 1ll);
   l.push_front(1l, 2u, 3ll);
 
-  erase(l, 1, 1ull, 1ll);
+  xl::erase(l, 1, 1ull, 1ll);
 
   //
   std::cout << "size: " << l.size() << std::endl;
