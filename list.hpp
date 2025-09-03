7ed2fda83d4357803433ccd6fd22192520222528
Janez Žemva <janezz55@gmail.com>
2025-09-03 16:15:17 +0200

    some fixes

--- list.hpp
+++ list.hpp
@@ -75,7 +75,8 @@ private:
     static void merge(const_iterator& b, const_iterator const m,
       decltype(b) e, auto c) noexcept(noexcept(c(*b, *b)))
     {
-      if ((m == e) || c(m.p_->v_, *m)) return;
+      if ((m == e) || c(m.p_->v_, *m) ||
+        (!c(m.p_->v_, *m) && !c(*m, m.p_->v_))) return;
 
       auto i(b), j(m), ni((c(*i, *j) ? ++i : (++j,
         i.p_ ? i.p_->l_ ^= detail::conv(i.n_, j.p_) : 0,
